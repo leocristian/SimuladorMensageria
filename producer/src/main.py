fceac95571693f9f0d106968107ca2f401937148
leocristian <leonardosclopes@gmail.com>
2022-09-07 17:44:35 -0300

    update producer

---
+++ producer/src/main.py
@@ -0,0 +1,48 @@
+import socket
+import random
+import string
+import sys
+
+class Producer:
+    def __init__(self, topic, rate):
+        self.topic = topic
+        self.rate = rate
+    
+    def getRandomMsg(self):
+        msgLen = random.randint(1, 1000)
+        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(msgLen))
+
+    def sendMessage(self, msg, address):
+        print("Sending...")
+        print(f"Msg topic: {msg['topic']}")
+        print(f"Msg body: {msg['body']}")
+
+        size = str(sys.getsizeof(str(msg)))
+        print(f"Lenght: {size}")
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR , 1)
+        self.server.connect(address)
+        self.send(str(msg).encode())
+
+    def start(self):
+        while True:
+            
+            address = ("localhost", 8000)
+
+            msgBody = self.getRandomMsg()
+            msg = {"topic": self.topic, "body": msgBody}
+
+            try:
+
+                self.sendMessage(msg, address)
+                time.sleep(1/self.rate)
+
+            except Exception as err:
+                print(f"Error: {err}")
+                break
+
+if __name__ == "__main__":
+    
+    producer1 = Producer("fanout", 100)
+    producer1.start()
