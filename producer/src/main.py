fc7e5f75da6012560ab5256c3b52b2cf744c103e
leocristian <leonardosclopes@gmail.com>
2022-09-11 17:24:54 -0300

    update producer

--- producer/src/main.py
+++ producer/src/main.py
@@ -1,13 +1,18 @@
 import socket
 import random
+import threading
 import string
+import time
 import sys
 
 class Producer:
     def __init__(self, topic, rate):
         self.topic = topic
         self.rate = rate
-    
+
+        self.thread = threading.Thread(target=self.startService())
+        self.thread.start()
+
     def getRandomMsg(self):
         msgLen = random.randint(1, 1000)
         return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(msgLen))
@@ -23,9 +28,11 @@ class Producer:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR , 1)
         self.server.connect(address)
-        self.send(str(msg).encode())
+        self.server.send(str(msg).encode())
 
-    def start(self):
+    def startService(self):
+
+        print(f"Thread: {threading.get_ident()} is running.")
         while True:
             
             address = ("localhost", 8000)
@@ -42,7 +49,10 @@ class Producer:
                 print(f"Error: {err}")
                 break
 
+    def run(self):
+        self.thread.join()
+
 if __name__ == "__main__":
     
-    producer1 = Producer("fanout", 100)
-    producer1.start()
+    producer1 = Producer("fanout", 1)
+    #producer1.run()
